cee8548d1ab544a6fdf3878d1fa17dc03acc9e37
Cornelius Venti <corneliusventi@gmail.com>
2022-11-02 00:32:46 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Cornelius Venti on 02/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
